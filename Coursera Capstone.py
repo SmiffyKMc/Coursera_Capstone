d1ef853bc2b0d9c391c8e5cb081b685cca63c064
Kieran McCarthy <kieran.mcc91@gmail.com>
2019-08-19 15:34:04 +0100

    Add files via upload

---
+++ Coursera Capstone.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Capstone
+
+# > This notebook will be for the Capstone on Coursera's Applied Data Science
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print('Hello Capstone Project Course!')
+
